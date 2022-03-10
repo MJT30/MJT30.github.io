da97cf6c6126e2179366e1b7e8584cc0f59ead5f
mylest913 <mylest913@gmail.com>
2022-03-09 21:18:45 -0800

    Zelda Trivia

---
+++ script.js
@@ -0,0 +1,17 @@
+const button1 = document.getElementById('button1')
+const button2 = document.getElementById('button2')
+const button3 = document.getElementById('button3')
+const button4 = document.getElementById('button4')
+let pointCounter = document.querySelector('.pointCounter')
+let promptBox = document.querySelector('.promptBox')
+
+
+let questions = {
+    question1 : ["What year did the orginal zelda game release?", "What time is it", "Testing"],
+
+    question2 : ["greetings",  "obni-wan", "We are testing this method", "Let's see how it goes"],
+
+    question3 : ["hello there", "hello myles", "we are working"],
+}
+
+
