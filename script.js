28b207eed03b1a7e6c76c958b04eb9bc477026b6
mylest913 <mylest913@gmail.com>
2022-03-16 22:27:24 -0700

    Zelda Trivia

--- script.js
+++ script.js
@@ -2,77 +2,75 @@
 // const button2 = document.getElementById('button2')
 // const button3 = document.getElementById('button3')
 // const button4 = document.getElementById('button4')
+const startButton = document.getElementById('startButton')
 const buttons = document.querySelectorAll('.button')
 let pointCounter = document.querySelector('.pointCounter')
 let promptBox = document.querySelector('.promptBox')
 // let buttonBoxes = document.getElementsByClassName('buttonBoxes')
-
-
+let questionNumber = 0
+let points = 0
+let correctAnswer = true
+let wrongAnswer = false
 //<------Questions------>//
 const questions = [
     {
-        question: ["The very first Legend of Zelda was released in 1986 on the NES"], 
-        answer : [ correct = true
-            // {text: 'A: 1986', correct: true},
-            // {text: 'B: 1984', correct: false},
-            // {text: 'C: 1989', correct: false},
-            // {text: 'D: 1991', correct: false}
-        ]
-    },
+        question: ["The very first Legend of Zelda was released in 1986 on the NES",    //0 also starts the game on a click. FIX later
+                   "The Legend of Zelda Breath of The Wild can technically be beaten in 30 minuets or less",   //1
+                   "In the Legend of Zelda Twilight Princess, Zelda is a playable chracter",   //2
+                   "During the section when you sneak in to the castle in OOT, what other Nintendo chracter can you see as a painting on the wall",  //3
+                   "The very first Legend of Zelda was released in 1986 on the NES", //4
+                   "Test question", //5
+                   "Test question", //6
+                   "Test question", //7
+                   "Test question", //8
+                   "Test question", //9
+                   "Test question", //10
+                   "Test question", //11
+                   "Test question", //12
+    
+    ] 
+        
+    }
 
-    // {
-    //     question: "In The Legend of Zelda Oricina of Time, What is the first song that you learn?",
-    //     answer: [
-    //         {text: 'Zeldas Lullaby', correct: false},
-    //         {text: 'Saras Song', correct: true},
-    //         {text: 'Song of Storms', correct: false},
-    //         {text: 'Ephonas Song', correct : false}
-
-    //     ],
-           
-    // },
-
-    // {
-    //     question: ["In The Legend of Zelda Oricina of Time, What is often described as the worst temple?"],
-    //     answer : [
-            
-    //     ]
-    // }
+ 
 ]
+
+
+
+function questionOne(){
+   
+
+}
+
+
 //<------ Question Function ------>//
-let correctAnswer 
+
 function questionPrompts(questionNum = 0) {
-    promptBox.innerText = questions[questionNum].question
-    correctAnswer = questions[questionNum].answer.filter((ans, i) => {if(ans.correct)return i})[0];
-       for(let correctAnswer = true; correctAnswer === true; correctAnswer++);{
-           alert(`That's right!`)
+   
        }
 
     
-    
-}
+
     
 questionPrompts()
 //<------ Question Function ------>
 
 //<------ Question Cycle Function ------>
+function nextQuestion() {
+    promptBox.innerText = questions[0].question[questionNumber]
+    
 
-function nextQuestion(questionNum = 0) {
-    // Event.preventDefault()
-    // if(correctAnswer === correctAnswer){
-    //     alert('Correct!')
-    //         console.log('please work');
-    // } 
+    
+    
+    
 }   
-nextQuestion()
-
-
 //<------Buttons------>//
 buttons.forEach((button, i) => button.addEventListener('click', () => clickMe(i)))
-
-
 function clickMe(i) {
     console.log(i);
+    questionNumber ++
+    nextQuestion()
+    questionOne()
 
 }
 //<------Buttons------>//
